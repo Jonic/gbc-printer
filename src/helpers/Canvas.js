52a0d779e735ad58d984c61f39f0ac911ac90e45
Jonic Linley <jonic@100yen.co.uk>
2019-06-15 01:57:32 +0100

    Wrote CanvasHelperMethods

--- src/helpers/Canvas.js
+++ src/helpers/Canvas.js
@@ -1,15 +1,42 @@
+const PIXEL_COLORS = ['#ffffff', '#aaaaaa', '#555555', '#000000']
+const TILE_SIZE = 8
+const TILE_X_INDEX_MAX = 20
 
-export CanvasInit = () => {
-  // Clear Screen
-  TILE_PIXEL_WIDTH = 8;
-  TILE_PIXEL_HEIGHT = 8;
-  colors = new Array("#ffffff", "#aaaaaa", "#555555", "#000000");
+export const CanvasDrawImage = ({ imageCanvas, imageData, pixelSize }) => {
+  const context = imageCanvas.current.getContext('2d')
+  context.clearRect(0, 0, imageCanvas.current.width, imageCanvas.current.height)
 
-  var canvas = document.getElementById("demo_canvas");
-  var ctx = canvas.getContext("2d");
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  let tileX = 0
+  let tileY = 0
 
-    /* Determine size of each pixel in canvas */
-    square_width = canvas.width / (TILE_PIXEL_WIDTH * TILES_PER_LINE);
-    square_height = square_width;
+  for (let tile of imageData) {
+    let tileXOffset = tileX * TILE_SIZE * pixelSize
+    let tileYOffset = tileY * TILE_SIZE * pixelSize
+
+    for (let bitY = 0; bitY < TILE_SIZE; bitY += 1) {
+      for (let bitX = 0; bitX < TILE_SIZE; bitX += 1) {
+        let bitValue = tile[bitY][bitX]
+        context.fillStyle = PIXEL_COLORS[bitValue]
+        context.fillRect(
+          tileXOffset + bitX * pixelSize,
+          tileYOffset + bitY * pixelSize,
+          pixelSize,
+          pixelSize,
+        )
+      }
+    }
+
+    tileX += 1
+
+    if (tileX === TILE_X_INDEX_MAX) {
+      tileX = 0
+      tileY += 1
+    }
+  }
 }
+
+const CanvasHelper = {
+  CanvasDrawImage,
+}
+
+export default CanvasHelper
