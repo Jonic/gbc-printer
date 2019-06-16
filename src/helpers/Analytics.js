5db1d0467998dd6b8712258b91894dbd98349558
Jonic Linley <jonic@100yen.co.uk>
2019-06-16 21:01:31 +0100

    Comment out console logs

--- src/helpers/Analytics.js
+++ src/helpers/Analytics.js
@@ -2,36 +2,32 @@ import ReactGA from 'react-ga'
 import config from 'config/analytics'
 
 const init = () => {
-  console.log('Analytics#init') // eslint-disable-line no-console
-
+  // console.log('Analytics#init') // eslint-disable-line no-console
   ReactGA.initialize(config.id)
 }
 
 const trackCameraDataProcess = () => {
   const event = {
-    category: 'Data',
     action: 'Camera Data Process',
+    category: 'Data',
   }
 
-  console.log('Data#trackCameraDataProcess', event) // eslint-disable-line no-console
-
+  // console.log('Data#trackCameraDataProcess', event) // eslint-disable-line no-console
   ReactGA.event(event)
 }
 
 const trackImageRender = () => {
   const event = {
-    category: 'Image',
     action: 'Image Render',
+    category: 'Image',
   }
 
-  console.log('Image#trackImageRender', event) // eslint-disable-line no-console
-
+  // console.log('Image#trackImageRender', event) // eslint-disable-line no-console
   ReactGA.event(event)
 }
 
 const trackPageView = () => {
-  console.log('Analytics#trackPageView') // eslint-disable-line no-console
-
+  // console.log('Analytics#trackPageView') // eslint-disable-line no-console
   ReactGA.set({ page: window.location.pathname })
   ReactGA.pageview(window.location.pathname)
 }
