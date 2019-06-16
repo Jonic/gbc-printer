5aab00505432896660e985273adcc32ed7139b32
Jonic Linley <jonic@100yen.co.uk>
2019-06-16 15:30:33 +0100

    Dev mode (#16)
    * Add devMode state var to AppContext
    
    * Add DevModeToggle component
    
    * Update snapshot
    
    * Tidy up data structure and delete non-dev data if !isDevMode

--- src/classes/CameraDataParser.js
+++ src/classes/CameraDataParser.js
@@ -1,18 +1,34 @@
 import Image from './Image'
 
 class CameraDataParser {
-  SPLIT_DELIMITER = '# Finished Pretending To Print for fun!'
+  SPLIT_DELIMITER = ' Finished Pretending To Print for fun!'
 
-  constructor({ cameraData }) {
-    this.prepareData(cameraData)
+  constructor({ cameraData, isDevMode }) {
+    this.isDevMode = isDevMode
+    this.rawData = cameraData
+
+    this.prepareData()
     this.decodeImages()
+    this.cleanUpData()
+  }
+
+  cleanUpData() {
+    if (this.isDevMode) {
+      return
+    }
+
+    delete this.rawData
+    delete this.cameraData
   }
 
   decodeImages() {
     this.images = []
 
-    for (let imageData of this.sourceCameraData) {
-      let image = new Image({ imageData })
+    for (let imageData of this.cameraData) {
+      let image = new Image({
+        imageData,
+        isDevMode: this.isDevMode,
+      })
 
       if (image.isValid()) {
         this.images.push(image)
@@ -20,9 +36,8 @@ class CameraDataParser {
     }
   }
 
-  prepareData(data) {
-    this.rawData = data
-    this.sourceCameraData = data.split(this.SPLIT_DELIMITER)
+  prepareData() {
+    this.cameraData = this.rawData.split(this.SPLIT_DELIMITER)
   }
 }
 
