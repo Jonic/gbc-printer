7afbe2fc9acfb4fbc0e48d3237d36c55cbf96597
Jonic Linley <jonic@100yen.co.uk>
2019-07-17 22:29:15 +0100

    Make look more good (#33)
    * Added some base styles
    
    * Create cera-round-pro-bold.woff2
    
    * Style it a bit
    
    * Enable picture download and removal

--- src/components/ImageCanvas/ImageCanvas.js
+++ src/components/ImageCanvas/ImageCanvas.js
@@ -1,11 +1,16 @@
-import React, { useContext, useEffect, useRef } from 'react'
+import './ImageCanvas.scss'
+
+import React, { useContext, useEffect, useRef, useState } from 'react'
 
 import AppContext from '../../contexts/App'
 import { CanvasDrawImage } from '../../helpers/Canvas'
 import PropTypes from 'prop-types'
 
-const ImageCanvas = ({ pixels }) => {
+const ImageCanvas = ({ index, pixels }) => {
   const { ignoreBorder, pixelSize } = useContext(AppContext)
+  const [canvasData, setCanvasData] = useState(null)
+  const [linkHref, setLinkHref] = useState(null)
+
   const imageCanvas = useRef(null)
   const imageElement = useRef(null)
 
@@ -13,24 +18,43 @@ const ImageCanvas = ({ pixels }) => {
     const canvas = imageCanvas.current
     const image = imageElement.current
 
-    CanvasDrawImage({
+    const data = CanvasDrawImage({
       canvas,
       ignoreBorder,
       image,
       pixels,
       pixelSize,
     })
+
+    setCanvasData(data)
   }, [ignoreBorder, imageCanvas, imageElement, pixels, pixelSize])
 
+  useEffect(() => {
+    setLinkHref(canvasData)
+  }, [canvasData])
+
   return (
-    <React.Fragment>
-      <img alt="" ref={imageElement} src="" />
-      <canvas ref={imageCanvas} />
-    </React.Fragment>
+    <div className="c-image-canvas">
+      {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+      <a
+        className="c-image-canvas__link"
+        download={`image_${index}.png`}
+        href={linkHref}
+      >
+        <img
+          alt=""
+          className="c-image-canvas__output"
+          ref={imageElement}
+          src=""
+        />
+        <canvas ref={imageCanvas} />
+      </a>
+    </div>
   )
 }
 
 ImageCanvas.propTypes = {
+  index: PropTypes.number.isRequired,
   pixels: PropTypes.array.isRequired,
 }
 
